b5b6fcd83b3b0307e29e0ca118ec2471f22b84d9
moameneslam <momoeslam0000@gmail.com>
2026-04-13 18:19:20 +0200

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react()],
@@ -14,3 +15,10 @@ export default defineConfig({
     }
   }
 })
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+});
